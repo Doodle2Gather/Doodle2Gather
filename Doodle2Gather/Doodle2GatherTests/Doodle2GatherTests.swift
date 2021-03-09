4cf1fe437e2addce89423d69acf3c315d86fe951
Hanming Zhu <hanming@huawen.com>
2021-03-09 15:23:02 +0800

    style: fix lint issues that come with default project

--- Doodle2Gather/Doodle2GatherTests/Doodle2GatherTests.swift
+++ Doodle2Gather/Doodle2GatherTests/Doodle2GatherTests.swift
@@ -1,33 +1,26 @@
-//
-//  Doodle2GatherTests.swift
-//  Doodle2GatherTests
-//
-//  Created by Hanming on 9/3/21.
-//
-
 import XCTest
 @testable import Doodle2Gather
 
 class Doodle2GatherTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+//    override func setUpWithError() throws {
+//        // Put setup code here. This method is called before the invocation of each test method in the class.
+//    }
+//
+//    override func tearDownWithError() throws {
+//        // Put teardown code here. This method is called after the invocation of each test method in the class.
+//    }
+//
+//    func testExample() throws {
+//        // This is an example of a functional test case.
+//        // Use XCTAssert and related functions to verify your tests produce the correct results.
+//    }
+//
+//    func testPerformanceExample() throws {
+//        // This is an example of a performance test case.
+//        self.measure {
+//            // Put the code you want to measure the time of here.
+//        }
+//    }
 
 }
