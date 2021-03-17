d7c14c864183b87e2cc0f6217f16fa5fd31b2c01
Christopher Goh <chrisgzf@gmail.com>
2021-03-17 13:13:29 +0800

    chore: Modularise git filters config

--- git_scripts/set_dynamic_bundle.sh
+++ git_scripts/set_dynamic_bundle.sh
@@ -1,15 +1,11 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 #  set_dynamic_bundle.sh
 #  Doodle2Gather
 #
 #  Created by Christopher Goh on 9/3/21.
 #  
+set -o nounset
+. git_scripts/config.sh
 
-ORIGINAL_DEV_TEAM="K9H85859Q4"
-NEW_DEV_TEAM="7E2G8573A5"
-ORIGINAL_BUNDLE_ID="com.hanmingdev.Doodle2Gather"
-NEW_BUNDLE_ID="sg.christopher.d2g"
-
-sed "s/DEVELOPMENT_TEAM = $ORIGINAL_DEV_TEAM;/DEVELOPMENT_TEAM = $NEW_DEV_TEAM;/g"
-sed "s/PRODUCT_BUNDLE_IDENTIFIER = $ORIGINAL_BUNDLE_ID;/PRODUCT_BUNDLE_IDENTIFIER = $NEW_BUNDLE_ID;/g"
+sed "s/DEVELOPMENT_TEAM = $ORIGINAL_DEV_TEAM;/DEVELOPMENT_TEAM = $NEW_DEV_TEAM;/g" | sed "s/PRODUCT_BUNDLE_IDENTIFIER = $ORIGINAL_BUNDLE_ID;/PRODUCT_BUNDLE_IDENTIFIER = $NEW_BUNDLE_ID;/g"
--- git_scripts/unset_dynamic_bundle.sh
+++ git_scripts/unset_dynamic_bundle.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 #  unset_dynamic_bundle.sh
 #  Doodle2Gather
@@ -6,10 +6,7 @@
 #  Created by Christopher Goh on 9/3/21.
 #
 
-ORIGINAL_DEV_TEAM="K9H85859Q4"
-NEW_DEV_TEAM="7E2G8573A5"
-ORIGINAL_BUNDLE_ID="com.hanmingdev.Doodle2Gather"
-NEW_BUNDLE_ID="sg.christopher.d2g"
+set -o nounset
+. git_scripts/config.sh
 
-sed "s/DEVELOPMENT_TEAM = $NEW_DEV_TEAM;/DEVELOPMENT_TEAM = $ORIGINAL_DEV_TEAM;/g"
-sed "s/PRODUCT_BUNDLE_IDENTIFIER = $NEW_BUNDLE_ID;/PRODUCT_BUNDLE_IDENTIFIER = $ORIGINAL_BUNDLE_ID;/g"
+sed "s/DEVELOPMENT_TEAM = $NEW_DEV_TEAM;/DEVELOPMENT_TEAM = $ORIGINAL_DEV_TEAM;/g" | sed "s/PRODUCT_BUNDLE_IDENTIFIER = $NEW_BUNDLE_ID;/PRODUCT_BUNDLE_IDENTIFIER = $ORIGINAL_BUNDLE_ID;/g"
