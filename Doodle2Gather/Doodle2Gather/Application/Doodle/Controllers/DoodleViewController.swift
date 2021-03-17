1e6336a3c0175c439db15ea59954442f8715fad2
Hanming Zhu <hanming@huawen.com>
2021-03-17 12:43:40 +0800

    feat: add and link new DoodleViewController

---
+++ Doodle2Gather/Doodle2Gather/Application/Doodle/Controllers/DoodleViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DoodleViewController.swift
+//  Doodle2Gather
+//
+//  Created by Hanming on 17/3/21.
+//
+
+import UIKit
+
+class DoodleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
