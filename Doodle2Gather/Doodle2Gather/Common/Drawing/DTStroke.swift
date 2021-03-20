3b446164bfa28e6c560520c9da582d2bad0f7784
Hanming Zhu <hanming@huawen.com>
2021-03-21 04:39:03 +0800

    feat: make DTStroke codable

--- Doodle2Gather/Doodle2Gather/Common/Drawing/DTStroke.swift
+++ Doodle2Gather/Doodle2Gather/Common/Drawing/DTStroke.swift
@@ -1,10 +1,12 @@
 import UIKit
+import PencilKit // TODO: Remove once placeholder below is removed.
 
-protocol DTStroke: Hashable {
+protocol DTStroke: Hashable, Codable {
     var color: UIColor { get set }
 
     /// Instantiates self using a generalised `DTStroke`.
     init<S: DTStroke>(from stroke: S)
+
 }
 
 // MARK: - Equatable
@@ -25,4 +27,27 @@ extension DTStroke {
         hasher.combine(color)
     }
 
+}
+
+// MARK: - Codable
+
+extension DTStroke {
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: DTStrokeCodingKeys.self)
+        try container.encode(CodableColor(uiColor: color), forKey: .color)
+    }
+
+    public init(from decoder: Decoder) throws {
+        // TODO: Remove this placeholder init once more properties have been added.
+        self.init(from: PKStroke(ink: .init(.marker, color: .black), path: .init()))
+        let container = try decoder.container(keyedBy: DTStrokeCodingKeys.self)
+        color = try container.decode(CodableColor.self, forKey: .color).uiColor
+    }
+
+}
+
+/// Keys for encoding and decoding `DTStroke`.
+enum DTStrokeCodingKeys: CodingKey {
+    case color
 }
