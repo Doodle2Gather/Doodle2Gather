36a7bb1e1b5d647727991b8e03930bda8f53845e
eksinyue <xinyuehehxy@gmail.com>
2021-03-20 22:19:22 +0800

    Fix swiftlint warnings

--- Doodle2Gather/Doodle2Gather/Application/WebSocket/WebSocketController.swift
+++ Doodle2Gather/Doodle2Gather/Application/WebSocket/WebSocketController.swift
@@ -8,99 +8,104 @@
 import Foundation
 
 struct AlertWrapper: Identifiable {
-  let id = UUID()
+    let id = UUID()
 }
 
 final class WebSocketController: ObservableObject {
-  @Published var actions: [UUID: NewDoodleActionResponse]
-  @Published var alertWrapper: AlertWrapper?
-  
-  private var id: UUID!
-  private let session: URLSession
-  var socket: URLSessionWebSocketTask!
-  private let decoder = JSONDecoder()
-  private let encoder = JSONEncoder()
-  
-  init() {
-    self.actions = [:]
-    self.alertWrapper = nil
-    
-    self.session = URLSession(configuration: .default)
-    self.connect()
-  }
-  
-  func connect() {
-    self.socket = session.webSocketTask(with: URL(string: "ws://localhost:8080/socket")!)
-    self.actions = [:]
-    self.listen()
-    self.socket.resume()
-  }
-    
-    func listen() {
-      self.socket.receive { [weak self] (result) in
-        guard let self = self else { return }
-        switch result {
-        case .failure(let error):
-          print(error)
-          return
-        case .success(let message):
-          switch message {
-          case .data(let data):
-            self.handle(data)
-          case .string(let str):
-            guard let data = str.data(using: .utf8) else { return }
-            self.handle(data)
-          @unknown default:
-            break
-          }
-        }
+    @Published var actions: [UUID: WebSocketTypes.NewDoodleActionResponse]
+    @Published var alertWrapper: AlertWrapper?
+
+    private var id: UUID!
+    private let session: URLSession
+    var socket: URLSessionWebSocketTask!
+    private let decoder = JSONDecoder()
+    private let encoder = JSONEncoder()
+
+    init() {
+        self.actions = [:]
+        self.alertWrapper = nil
+
+        self.session = URLSession(configuration: .default)
+        self.connect()
+    }
+
+    func connect() {
+        self.socket = session.webSocketTask(with: URL(string: "ws://localhost:8080/socket")!)
+        self.actions = [:]
         self.listen()
-      }
+        self.socket.resume()
     }
-  
-  func addAction(_ content: String) {
-    guard let id = self.id else { return }
-    let message = NewDoodleActionMessage(id: id, content: content)
-    do {
-      let data = try encoder.encode(message)
-      self.socket.send(.data(data)) { (err) in
-        if err != nil {
-          print(err.debugDescription)
+
+    func listen() {
+        self.socket.receive { [weak self] result in
+            guard let self = self else {
+                return
+            }
+            switch result {
+            case .failure(let error):
+                print(error)
+                return
+            case .success(let message):
+                switch message {
+                case .data(let data):
+                    self.handle(data)
+                case .string(let str):
+                    guard let data = str.data(using: .utf8) else {
+                        return
+                    }
+                    self.handle(data)
+                @unknown default:
+                    break
+                }
+            }
+            self.listen()
         }
-      }
-    } catch {
-      print(error)
     }
-  }
-  
-  func handle(_ data: Data) {
-    do {
-      let decodedData = try decoder.decode(DoodleActionMessageData.self, from: data)
-      switch decodedData.type {
-      case .handshake:
-        print("Shook the hand")
-        let message = try decoder.decode(DoodleActionHandShake.self, from: data)
-        self.id = message.id
-      case .response:
-        try self.handleQuestionResponse(data)
-      default:
-        break
-      }
-    } catch {
-      print(error)
-    }
-  }
 
-  func handleQuestionResponse(_ data: Data) throws {
-    // 1
-    let response = try decoder.decode(NewDoodleActionResponse.self, from: data)
-    DispatchQueue.main.async {
-      if response.success, let id = response.id {
-        self.actions[id] = response
-      } else {
-        print(response.message)
-      }
+    func addAction(_ content: String) {
+        guard let id = self.id else {
+            return
+        }
+        let message = WebSocketTypes.NewDoodleActionMessage(id: id, content: content)
+        do {
+            let data = try encoder.encode(message)
+            self.socket.send(.data(data)) { err in
+                if err != nil {
+                    print(err.debugDescription)
+                }
+            }
+        } catch {
+            print(error)
+        }
     }
-  }
+
+    func handle(_ data: Data) {
+        do {
+            let decodedData = try decoder.decode(WebSocketTypes.DoodleActionMessageData.self, from: data)
+            switch decodedData.type {
+            case .handshake:
+                print("Shook the hand")
+                let message = try decoder.decode(WebSocketTypes.DoodleActionHandShake.self, from: data)
+                self.id = message.id
+            case .response:
+                try self.handleQuestionResponse(data)
+            default:
+                break
+            }
+        } catch {
+            print(error)
+        }
+    }
+
+    func handleQuestionResponse(_ data: Data) throws {
+        // 1
+        let response = try decoder.decode(WebSocketTypes.NewDoodleActionResponse.self, from: data)
+        DispatchQueue.main.async {
+            if response.success, let id = response.id {
+                self.actions[id] = response
+            } else {
+                print(response.message)
+            }
+        }
+    }
 }
-
