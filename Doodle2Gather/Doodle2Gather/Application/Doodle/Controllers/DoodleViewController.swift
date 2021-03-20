e7b65597648b1ef907a8588f35346141d0d072b6
Hanming Zhu <hanming@huawen.com>
2021-03-21 05:19:35 +0800

    feat: link DTCanvasViewController to Doodle storyboard

--- Doodle2Gather/Doodle2Gather/Application/Doodle/Controllers/DoodleViewController.swift
+++ Doodle2Gather/Doodle2Gather/Application/Doodle/Controllers/DoodleViewController.swift
@@ -2,20 +2,39 @@ import UIKit
 
 class DoodleViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    private var canvasController: CanvasController?
 
-        // Do any additional setup after loading the view.
+    // Using `static let` in enums for constants seems to have the following advantages:
+    // https://stackoverflow.com/a/61543705
+    enum Segues {
+        static let toCanvas = "ToCanvas"
     }
 
-    /*
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        switch segue.identifier {
+        case Segues.toCanvas:
+            guard let destination = segue.destination as? DTCanvasViewController else {
+                return
+            }
+            destination.delegate = self
+            // TODO: Complete injection of drawing into subcontroller
+            // destination.drawing = // Inject drawing
+            self.canvasController = destination
+        default:
+            return
+        }
     }
-    */
 
+}
+
+// MARK: - CanvasControllerDelegate
+
+extension DoodleViewController: CanvasControllerDelegate {
+    func actionDidFinish(action: DTAction) {
+        // TODO: Dispatch this action via the network
+    }
+
+    // TODO: Add dispatching of actions received from network to the canvas
 }
