8e0e2893ebbf2e69d7aec83082a8cebc5b44cdb1
Christopher Goh <chrisgzf@gmail.com>
2021-03-21 06:12:59 +0800

    feat: Add login route

---
+++ Doodle2Gather-websocket-backend/Sources/App/Models/LoginResponse.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  
+//
+//  Created by Christopher Goh on 21/3/21.
+//
+
+import Fluent
+import Vapor
+
+struct LoginResponse: Content {
+    var roomId = "devRoom"
+}
--- Doodle2Gather-websocket-backend/Sources/App/routes.swift
+++ Doodle2Gather-websocket-backend/Sources/App/routes.swift
@@ -16,4 +16,16 @@ func routes(_ app: Application) throws {
         }
     }
     
+    app.post("login") { req -> LoginResponse in
+        do {
+            let loginRequest =  try req.content.decode(LoginRequest.self)
+            guard loginRequest.password == "goodpassword" else {
+                throw Abort(.unauthorized, reason: "Invalid credentials")
+            }
+            return LoginResponse()
+        } catch is DecodingError {
+            throw Abort(.badRequest, reason: "Request must have username and password")
+        }
+    }
+    
 }
