56727554765eb20e865856582eadcc5207ad9ef2
Hanming Zhu <hanming@huawen.com>
2021-03-21 18:46:37 +0800

    feat: update DTStroke to contain DTPoints

--- Doodle2Gather/Doodle2Gather/Common/Doodling/DTStroke.swift
+++ Doodle2Gather/Doodle2Gather/Common/Doodling/DTStroke.swift
@@ -1,11 +1,17 @@
 import UIKit
-import PencilKit // TODO: Remove once placeholder below is removed.
 
+/// Represents a single stroke, consisting of multiple points.
 protocol DTStroke: Hashable, Codable {
+    associatedtype Point: DTPoint
+
     var color: UIColor { get set }
+    var tool: DTTool { get set }
+    var points: Set<Point> { get set }
 
     /// Instantiates self using a generalised `DTStroke`.
     init<S: DTStroke>(from stroke: S)
+
+    init<P: DTPoint>(color: UIColor, tool: DTTool, points: Set<P>)
 
 }
 
@@ -14,7 +20,7 @@ protocol DTStroke: Hashable, Codable {
 extension DTStroke {
 
     public static func == (lhs: Self, rhs: Self) -> Bool {
-        lhs.color == rhs.color
+        lhs.color == rhs.color && lhs.tool == rhs.tool && lhs.points == rhs.points
     }
 
 }
@@ -25,6 +31,8 @@ extension DTStroke {
 
     public func hash(into hasher: inout Hasher) {
         hasher.combine(color)
+        hasher.combine(tool)
+        hasher.combine(points)
     }
 
 }
@@ -36,13 +44,17 @@ extension DTStroke {
     public func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: DTStrokeCodingKeys.self)
         try container.encode(CodableColor(uiColor: color), forKey: .color)
+        try container.encode(tool.rawValue, forKey: .tool)
+        try container.encode(points, forKey: .points)
     }
 
     public init(from decoder: Decoder) throws {
-        // TODO: Remove this placeholder init once more properties have been added.
-        self.init(from: PKStroke(ink: .init(.marker, color: .black), path: .init()))
         let container = try decoder.container(keyedBy: DTStrokeCodingKeys.self)
-        color = try container.decode(CodableColor.self, forKey: .color).uiColor
+        let color = try container.decode(CodableColor.self, forKey: .color).uiColor
+        let tool = DTTool(rawValue: try container.decode(String.self, forKey: .tool)) ?? .pen
+        let points = try container.decode(Set<Point>.self, forKey: .points)
+
+        self.init(color: color, tool: tool, points: points)
     }
 
 }
@@ -50,4 +62,6 @@ extension DTStroke {
 /// Keys for encoding and decoding `DTStroke`.
 enum DTStrokeCodingKeys: CodingKey {
     case color
+    case tool
+    case points
 }
