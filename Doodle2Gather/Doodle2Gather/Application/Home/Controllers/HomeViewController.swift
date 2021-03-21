33fd710c47037fc7672a3497ce156fbc2262360c
Christopher Goh <chrisgzf@gmail.com>
2021-03-21 20:36:25 +0800

    fix: fix lint

--- Doodle2Gather/Doodle2Gather/Application/Home/Controllers/HomeViewController.swift
+++ Doodle2Gather/Doodle2Gather/Application/Home/Controllers/HomeViewController.swift
@@ -5,10 +5,12 @@ class HomeViewController: UIViewController {
     @IBOutlet private var passwordTextField: UITextField!
     @IBOutlet private var roomNameTextField: UITextField!
     @IBOutlet private var loginButton: UIStackView!
+    @IBOutlet private var errorMessageLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        errorMessageLabel.text = ""
         usernameTextField.text = UIDevice.current.name
         passwordTextField.text = "goodpassword"
         roomNameTextField.text = "devRoom"
@@ -26,11 +28,12 @@ class HomeViewController: UIViewController {
 
     @IBAction private func onLoginTapped(_ sender: UIButton) {
         attemptLogin()
-        performSegue(withIdentifier: "goToDoodle", sender: self)
     }
 
     private func attemptLogin() {
-        let params = ["username": usernameTextField.text, "password": passwordTextField.text, "roomName": roomNameTextField.text]
+        let params = ["username": usernameTextField.text,
+                      "password": passwordTextField.text,
+                      "roomName": roomNameTextField.text]
         let url = URL(string: "http://d2g.christopher.sg:8080/login")!
         var request = URLRequest(url: url)
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
@@ -40,24 +43,44 @@ class HomeViewController: UIViewController {
         do {
             request.httpBody = try JSONSerialization.data(withJSONObject: params)
         } catch {
-            print(error.localizedDescription)
+            DispatchQueue.main.async {
+                self.errorMessageLabel.text = error.localizedDescription
+            }
         }
 
         let task = URLSession.shared.dataTask(with: request, completionHandler: { data, response, error in
           if let error = error {
-            print("Error logging in \(error)")
+            DispatchQueue.main.async {
+                self.errorMessageLabel.text = error.localizedDescription
+            }
             return
           }
 
           guard let httpResponse = response as? HTTPURLResponse,
                 (200...299).contains(httpResponse.statusCode) else {
-            print("Error with the response, unexpected status code: \(String(describing: response))")
+                DispatchQueue.main.async {
+                    self.errorMessageLabel.text = "Incorrect credentials"
+                }
             return
           }
 
-          if let data = data,
-             let loginResponse = try? JSONDecoder().decode(LoginResponse.self, from: data) {
-            print(loginResponse.roomId)
+            guard let data = data else {
+                DispatchQueue.main.async {
+                    self.errorMessageLabel.text = "Unable to fetch response. Is your internet connection working?"
+                }
+                return
+            }
+
+            do {
+                try JSONDecoder().decode(LoginResponse.self, from: data)
+            } catch {
+                DispatchQueue.main.async {
+                    self.errorMessageLabel.text = "Unable to fetch response. Is your internet connection working?"
+                }
+                return
+            }
+            DispatchQueue.main.async {
+                self.performSegue(withIdentifier: "goToDoodle", sender: self)
             }
         })
         task.resume()
