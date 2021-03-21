4b2c93566a911385e568ca653b5a4a716201fa5d
Hanming Zhu <hanming@huawen.com>
2021-03-21 18:47:39 +0800

    feat: make PKStrokePoint adopt the DTPoint protocol

---
+++ Doodle2Gather/Doodle2Gather/Common/Extensions/PKStrokePoint+DTPoint.swift
@@ -0,0 +1,8 @@
+import PencilKit
+
+extension PKStrokePoint: DTPoint {
+    init<P>(from point: P) where P: DTPoint {
+        self.init(location: point.location, timeOffset: point.timeOffset, size: point.size, opacity: point.opacity,
+                  force: point.force, azimuth: point.azimuth, altitude: point.altitude)
+    }
+}
