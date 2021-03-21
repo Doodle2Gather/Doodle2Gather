c525c013d2eabadcbfffbc639d8f6fe0801712cb
Hanming Zhu <hanming@huawen.com>
2021-03-21 19:25:47 +0800

    refactor: rename CodableColor to DTCodableColor

--- Doodle2Gather/Doodle2Gather/Common/Doodling/DTCodableColor.swift
+++ Doodle2Gather/Doodle2Gather/Common/Doodling/DTCodableColor.swift
@@ -1,7 +1,7 @@
 import UIKit
 
 /// Adapted from https://stackoverflow.com/a/50934846
-struct CodableColor: Codable {
+struct DTCodableColor: Codable {
     var red: CGFloat = 0.0, green: CGFloat = 0.0, blue: CGFloat = 0.0, alpha: CGFloat = 0.0
 
     var uiColor: UIColor {
