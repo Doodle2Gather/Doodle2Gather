ecd4e5453bb5400e035f021b1bf9e5f3d5afb3eb
Hanming Zhu <hanming@huawen.com>
2021-03-21 19:11:55 +0800

    style: remove trailing whitespace

--- Doodle2Gather/Doodle2Gather/Common/Doodling/DTCanvasViewDelegate.swift
+++ Doodle2Gather/Doodle2Gather/Common/Doodling/DTCanvasViewDelegate.swift
@@ -1,7 +1,7 @@
 /// Delegate for a `DTCanvasView`.
 /// It is notified when a doodle for the canvas is changed.
 protocol DTCanvasViewDelegate: AnyObject {
-    
+
     /// Notifies the delegate that the user has stopped drawing with the tool.
     func canvasViewDidEndUsingTool(_: DTCanvasView)
 
