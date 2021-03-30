c3e2d1766adf351b74b84c29af7056e4244c8a73
Hanming Zhu <hanming@huawen.com>
2021-03-30 15:20:10 +0800

    chore: shift github_scripts into .github/scripts

--- .github/scripts/unset_dynamic_bundle.sh
+++ .github/scripts/unset_dynamic_bundle.sh
@@ -7,6 +7,6 @@
 #
 
 set -o nounset
-. git_scripts/config.sh
+. .github/scripts/config.sh
 
 sed "s/DEVELOPMENT_TEAM = $NEW_DEV_TEAM;/DEVELOPMENT_TEAM = $ORIGINAL_DEV_TEAM;/g" | sed "s/PRODUCT_BUNDLE_IDENTIFIER = $NEW_BUNDLE_ID;/PRODUCT_BUNDLE_IDENTIFIER = $ORIGINAL_BUNDLE_ID;/g"
