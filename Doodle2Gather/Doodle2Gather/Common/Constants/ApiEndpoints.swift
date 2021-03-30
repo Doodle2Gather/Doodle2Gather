bf3ca2953b066f0aaf99145536c70756ff5161f8
Christopher Goh <chrisgzf@gmail.com>
2021-03-30 08:28:23 +0800

    feat: Use same token server as rtm for rtc

--- Doodle2Gather/Doodle2Gather/Common/Constants/ApiEndpoints.swift
+++ Doodle2Gather/Doodle2Gather/Common/Constants/ApiEndpoints.swift
@@ -1,5 +1,5 @@
 enum ApiEndpoints {
     static let Login = "https://d2g.christopher.sg/login"
-    static let AgoraTokenServer = "http://z.christopher.sg:2703/access_token"
+    static let AgoraTokenServer = "https://d2g.christopher.sg/rtcToken"
     static let Room = "wss://d2g.christopher.sg/rooms/devRoom"
 }
