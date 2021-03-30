3a305a24313634a08452dbc5940a712c414c4c2c
Christopher Goh <chrisgzf@gmail.com>
2021-03-30 08:17:20 +0800

    feat: Use HTTPS and WSS endpoints

--- Doodle2Gather/Doodle2Gather/Common/Constants/ApiEndpoints.swift
+++ Doodle2Gather/Doodle2Gather/Common/Constants/ApiEndpoints.swift
@@ -1,5 +1,5 @@
 enum ApiEndpoints {
-    static let Login = "http://d2g.christopher.sg:8080/login"
+    static let Login = "https://d2g.christopher.sg/login"
     static let AgoraTokenServer = "http://z.christopher.sg:2703/access_token"
-    static let Room = "ws://d2g.christopher.sg:8080/rooms/devRoom"
+    static let Room = "wss://d2g.christopher.sg/rooms/devRoom"
 }
