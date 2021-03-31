004de853173f5132af7fd10d71c464b00a82ab27
Hanming Zhu <hanming@huawen.com>
2021-03-31 08:34:19 +0800

    fix: shift DoodlingLibrary into current directory

---
+++ DoodlingLibrary/DoodlingLibrary/Models/DTDoodle.swift
@@ -0,0 +1,14 @@
+/// Represents a doodle that contains strokes and can be rendered.
+public protocol DTDoodle {
+    associatedtype Stroke: DTStroke
+    var dtStrokes: Set<Stroke> { get set }
+
+    /// Instantiates self using a generalised `DTDoodle`.
+    init<D: DTDoodle>(from doodle: D)
+
+    /// Removes strokes in the given set of strokes from the current set.
+    mutating func removeStrokes<S: DTStroke>(_: Set<S>)
+
+    /// Adds strokes in the given set of strokes to the current set.
+    mutating func addStrokes<S: DTStroke>(_: Set<S>)
+}
---
+++ DoodlingLibrary/DoodlingLibrary/Models/DTTool.swift
@@ -0,0 +1,8 @@
+/// Tool used on the canvas. Currently it is an enum, but
+/// will explore custom tools in the future.
+public enum DTTool: String {
+    case pen
+    case pencil
+    case marker
+    case eraser
+}
