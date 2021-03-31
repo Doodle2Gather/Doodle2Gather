0e7da909b701ff8c940b5fcfc85cd8d801e8640f
Hanming Zhu <hanming@huawen.com>
2021-03-31 15:21:57 +0800

    refactor: convert DTDoodle to hold an array of DTStrokes

--- DoodlingLibrary/DoodlingLibrary/Models/DTDoodle.swift
+++ DoodlingLibrary/DoodlingLibrary/Models/DTDoodle.swift
@@ -1,14 +1,14 @@
 /// Represents a doodle that contains strokes and can be rendered.
 public protocol DTDoodle {
     associatedtype Stroke: DTStroke
-    var dtStrokes: Set<Stroke> { get set }
+    var dtStrokes: [Stroke] { get set }
 
     /// Instantiates self using a generalised `DTDoodle`.
     init<D: DTDoodle>(from doodle: D)
 
-    /// Removes strokes in the given set of strokes from the current set.
+    /// Removes strokes in the given set of strokes from the current array of strokes.
     mutating func removeStrokes<S: DTStroke>(_: Set<S>)
 
-    /// Adds strokes in the given set of strokes to the current set.
+    /// Adds strokes in the given set of strokes to the current array of strokes.
     mutating func addStrokes<S: DTStroke>(_: Set<S>)
 }
