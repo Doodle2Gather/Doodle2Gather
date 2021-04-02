ee28f0511cea51d78a846589b7401ca1861b5cb6
Hanming Zhu <hanming@huawen.com>
2021-04-02 14:29:30 +0800

    fix: merge remaining file

--- DoodlingLibrary/DoodlingLibrary/Models/DTDoodle.swift
+++ DoodlingLibrary/DoodlingLibrary/Models/DTDoodle.swift
@@ -6,17 +6,9 @@ public protocol DTDoodle {
     /// Instantiates self using a generalised `DTDoodle`.
     init<D: DTDoodle>(from doodle: D)
 
-<<<<<<< HEAD
     /// Removes strokes in the given array of strokes from the current array of strokes.
     mutating func removeStrokes<S: DTStroke>(_: [S])
 
     /// Adds strokes in the given array of strokes to the current array of strokes.
     mutating func addStrokes<S: DTStroke>(_: [S])
-=======
-    /// Removes strokes in the given set of strokes from the current array of strokes.
-    mutating func removeStrokes<S: DTStroke>(_: Set<S>)
-
-    /// Adds strokes in the given set of strokes to the current array of strokes.
-    mutating func addStrokes<S: DTStroke>(_: Set<S>)
->>>>>>> d9b362340cf393cf00901e5caf4741bf87897f82
 }
