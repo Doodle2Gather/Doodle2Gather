8d6fca4ed4687078c9c8726dee198f1c5bf94e52
Christopher Goh <chrisgzf@gmail.com>
2021-04-04 04:49:42 +0800

    feat: Complete login and sign up

--- Doodle2Gather/Doodle2Gather/Application/Home/Controllers/HomeViewController.swift
+++ Doodle2Gather/Doodle2Gather/Application/Home/Controllers/HomeViewController.swift
@@ -45,57 +45,23 @@ class HomeViewController: UIViewController {
     }
 
     @IBAction private func onSubmitButtonTapped(_ sender: UIButton) {
-        attemptLogin()
+        let segment = Segment(rawValue: formActionSegmentedControl.selectedSegmentIndex)
+        switch segment {
+        case .login:
+            attemptLogin()
+        case .register:
+            attemptRegister()
+        default:
+            fatalError("Invalid segment")
+        }
+    }
+
+    private func attemptRegister() {
+        DTAuth.signUp(email: emailTextField.text!, password: passwordTextField.text!, displayName: emailTextField.text!)
     }
 
     private func attemptLogin() {
-        DTAuth.signUp(email: emailTextField.text!, password: passwordTextField.text!, displayName: emailTextField.text!)
-//        let params = ["username": usernameTextField.text,
-//                      "password": passwordTextField.text,
-//                      "roomName": roomNameTextField.text]
-//        let url = URL(string: ApiEndpoints.Login)!
-//        var request = URLRequest(url: url)
-//        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-//        request.addValue("application/json", forHTTPHeaderField: "Accept")
-//        request.httpMethod = "POST"
-//
-//        do {
-//            request.httpBody = try JSONSerialization.data(withJSONObject: params)
-//        } catch {
-//            DispatchQueue.main.async {
-//                self.errorMessageLabel.text = error.localizedDescription
-//            }
-//        }
-//
-//        let task = URLSession.shared.dataTask(with: request, completionHandler: { data, response, error in
-//            if let error = error {
-//                DispatchQueue.main.async {
-//                    self.errorMessageLabel.text = error.localizedDescription
-//                }
-//                return
-//            }
-//
-//            guard let httpResponse = response as? HTTPURLResponse,
-//                  (200...299).contains(httpResponse.statusCode) else {
-//                DispatchQueue.main.async {
-//                    self.errorMessageLabel.text = ErrorMessages.incorrectCredentials
-//                }
-//                return
-//            }
-//
-//            guard let data = data,
-//                  (try? JSONDecoder().decode(LoginResponse.self, from: data)) != nil else {
-//                DispatchQueue.main.async {
-//                    self.errorMessageLabel.text = ErrorMessages.unableToFetchResponse
-//                }
-//                return
-//            }
-//
-//            DispatchQueue.main.async {
-//                self.performSegue(withIdentifier: "goToDoodle", sender: self)
-//            }
-//        })
-//        task.resume()
+        DTAuth.login(email: emailTextField.text!, password: passwordTextField.text!)
     }
 }
 
@@ -108,6 +74,12 @@ extension HomeViewController: DTAuthDelegate {
     }
 
     func loginDidSucceed() {
+        DTLogger.event("""
+User Logged in
+ - Display Name: \(DTAuth.user?.displayName ?? "Not found")
+ - UID: \(DTAuth.user?.uid ?? "Not found")
+ - Email: \(DTAuth.user?.email ?? "Not found")
+""")
         DispatchQueue.main.async {
             self.performSegue(withIdentifier: "goToDoodle", sender: self)
         }
