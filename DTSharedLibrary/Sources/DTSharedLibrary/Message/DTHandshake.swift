494060cf0b36fec6a2411b39d03d22bbd215cada
Christopher Goh <chrisgzf@gmail.com>
2021-04-17 23:25:08 +0800

    revert: DTHandshake send uuid of WS only

--- DTSharedLibrary/Sources/DTSharedLibrary/Message/DTHandshake.swift
+++ DTSharedLibrary/Sources/DTSharedLibrary/Message/DTHandshake.swift
@@ -3,10 +3,8 @@ import Foundation
 public struct DTHandshake: Codable {
     var type = DTRoomMessageType.handshake
     public let id: UUID
-    public let users: [DTAdaptedUser]
 
-    public init(id: UUID, users: [DTAdaptedUser]) {
+    public init(id: UUID) {
         self.id = id
-        self.users = users
     }
 }
