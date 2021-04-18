1e5d55951765d395b5456c88a5e444eb783524bc
Hanming Zhu <hanming@huawen.com>
2021-04-18 14:19:20 +0800

    refactor: move socket controller protocol files

---
+++ Doodle2Gather/Doodle2Gather/Adapters/WebSocket/SocketControllerDelegate.swift
@@ -0,0 +1,18 @@
+import Foundation
+import DTSharedLibrary
+
+protocol SocketControllerDelegate: AnyObject {
+
+    /// Tells the delegate that an action was received and should be dispatched.
+    func dispatchAction(_ action: DTAction)
+
+    /// Tells the delegate to load the current doodles.
+    func loadDoodles(_ doodles: [DTAdaptedDoodle])
+
+    /// Tells the delegate to add a new doodle.
+    func addNewDoodle(_ doodle: DTAdaptedDoodle)
+
+    /// Tells the delegate to remove the doodle with the given UUID.
+    func removeDoodle(doodleId: UUID)
+
+}
