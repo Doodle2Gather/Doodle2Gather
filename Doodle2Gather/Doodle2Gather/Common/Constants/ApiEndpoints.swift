3ebd94f49d844697c8d9d2588cb00cb5bd12435b
Christopher Goh <chrisgzf@gmail.com>
2021-04-18 13:41:22 +0800

    feat: Use prod endpoints on main, add scripts

--- Doodle2Gather/Doodle2Gather/Common/Constants/ApiEndpoints.swift
+++ Doodle2Gather/Doodle2Gather/Common/Constants/ApiEndpoints.swift
@@ -4,9 +4,11 @@ enum ApiEndpoints {
     static let AgoraRtmTokenServer = "https://d2g.christopher.sg/rtmToken"
     static let Room = { (roomId: String, userId: String) in "wss://d2g.christopher.sg/rooms/\(roomId)/\(userId)" }
     static let Api = "https://d2g.christopher.sg/api"
+    static let WS = "wss://d2g.christopher.sg"
 
     // for testing only
     static let localRoom = { (roomId: String, userId: String) in "ws://localhost:8080/rooms/\(roomId)/\(userId)" }
     static let localApi = "http://localhost:8080/api/"
     static let localWS = "ws://localhost:8080"
 }
+
---
+++ use_local_backend.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+localIp=$(ipconfig getifaddr en0)
+apiEndpointsPath="./Doodle2Gather/Doodle2Gather/Common/Constants/ApiEndpoints.swift"
+
+echo "Your local IP is: $localIp"
+
+sed -i '' "s/localhost/$localIp/g" $apiEndpointsPath
+LC_ALL=C find . -type f -name '*.swift' -exec sed -i '' 's/ApiEndpoints\.Api/ApiEndpoints\.localApi/g' {} +
+LC_ALL=C find . -type f -name '*.swift' -exec sed -i '' 's/ApiEndpoints\.WS/ApiEndpoints\.localWS/g' {} +
