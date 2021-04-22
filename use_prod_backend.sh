823713bbf11e2275bdc6697a78c2c009f7ebaba6
Hanming Zhu <hanming@huawen.com>
2021-04-22 12:41:17 +0800

    Revert "chore: shift files around"
    This reverts commit 5d57bc6e94c183e43a67ffb4e111ef5fc7eb1aba.

--- use_prod_backend.sh
+++ use_prod_backend.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 localIp=$(ipconfig getifaddr en0)
-apiEndpointsPath="../Doodle2Gather/Doodle2Gather/Common/Constants/ApiEndpoints.swift"
+apiEndpointsPath="./Doodle2Gather/Doodle2Gather/Common/Constants/ApiEndpoints.swift"
 
 sed -i '' "s/$localIp/localhost/g" $apiEndpointsPath
 LC_ALL=C find . -type f -name '*.swift' -exec sed -i '' 's/ApiEndpoints\.localApi/ApiEndpoints\.Api/g' {} +
